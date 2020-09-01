6c43196208c77f9c74e3749767760455eed92c97
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-09-01 18:52:49 +0200

    Add registerForm

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class RegisterForm extends Component {
+  state = { data: { firstname: "", secondname ="",email = "", phone= "",  password: "" }, errors: {} };
+  render() {
+    return null;
+  }
+}
+
+export default RegisterForm;
